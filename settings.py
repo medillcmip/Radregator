c2a655583d0fa1436a353fa2389e36d57965cf03
Geoffrey Hing <ghing@geoffsnotebook.(none)>
2010-10-13 10:46:12 -0500

    started getting base templates up and running.

--- settings.py
+++ settings.py
@@ -1,4 +1,5 @@
 # Django settings for radregator project.
+import os.path
 
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
@@ -61,6 +62,7 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
+    os.path.join(os.path.basename(__file__), 'templates'),
 )
 
 INSTALLED_APPS = (
