f3f851b207157b056ee630862f5f0ff2ef125109
Geoffrey Hing <ghing@geoffsnotebook.(none)>
2010-10-14 17:03:12 -0500

    Created slugify utility function to generate slug from a string.
    I put this in a new util module.  This will be the place to put utility
    functions used throughout core.

---
+++ core/utils.py
@@ -0,0 +1,10 @@
+"""Utility functions that may be used throughout the core application."""
+
+import re
+
+def slugify(s):
+    p = re.compile('\W') # match non-alphanumeric characters 
+    new_s = p.sub('-', s) # and replace them with '-'
+    new_s = new_s.lower() # and then convert to lowercase
+
+    return new_s
