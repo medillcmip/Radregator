7f9046766727d536e87f3406283009c3c55ecc63
Steven Melendez <smelendez@wireless-165-124-143-115.nuwlan.northwestern.edu>
2010-10-14 11:14:40 -0500

    Added skeleton front page view/comment submit form

--- core/forms.py
+++ core/forms.py
@@ -1,4 +1,19 @@
 from django import forms
+from django.forms.widgets import Select
+from radregator.core.models import Comment,CommentType
+
+class CommentSubmitForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+
+        fields = ('text', 'comment_type', 'tags')
+        comment_types = [ct.name for ct in CommentType.objects.all()]
+        widgets =  {
+            'comment_type' : Select(choices = comment_types)}
+
+
+
+
 
 
 
--- core/views.py
+++ core/views.py
@@ -2,7 +2,29 @@ from django.http import HttpResponseRedirect, HttpResponse
 from django.shortcuts import render_to_response
 from django.conf import settings
 from fbapi.facebook import *
+from models import Topic
+from radregator.core.forms import CommentSubmitForm
 
+def frontpage(request):
+    """ Front page demo"""
+
+    if request.method == 'POST':
+        pass
+
+    template_dict = {}
+
+    topics = Topics.objects.all()[:5] # Will want to filter, order in later versions
+
+    template_dict['topics'] = topics
+    template_dict['comment_form'] = CommentSubmitForm()
+
+    return render_to_response('frontpage.html', template_dict)
+
+
+
+
+    
+    return render_to_response('frontpage.html', template_dict)
 def index(request):
     """Default view."""
     template_dict = {}
