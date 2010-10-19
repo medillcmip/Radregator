5218fccf95747e16d261bfffb441deac3b5b7fd0
Geoffrey Hing <ghing@geoffsnotebook.(none)>
2010-10-19 11:11:55 -0500

    Added NonAjaxRequest exception to be used by my API views.

--- core/exceptions.py
+++ core/exceptions.py
@@ -4,3 +4,9 @@ class UnknownOutputFormatException(Exception):
     """Exception thrown when an API call is made requesting output in
        an unknown format."""
     pass
+
+class NonAjaxRequest(Exception):
+    """Exception thrown when an API call is made that is not an AJAX request
+       and we don't want to allow this kind of (potentially) cross-site 
+       call"""
+    pass
