9fa6c9d55a60e15af0b38eb5a1ccde915cd727f6
Geoffrey Hing <ghing@geoffsnotebook.(none)>
2010-10-19 04:06:51 -0500

    Added __unicode__ method to CommentResponse

--- core/models.py
+++ core/models.py
@@ -116,3 +116,6 @@ class CommentResponse(models.Model):
     comment = models.ForeignKey(Comment)
     user = models.ForeignKey(User)
     type = models.CharField(max_length=20, choices=COMMENT_RESPONSE_CHOICES)
+
+    def __unicode__(self):
+        return "%s %s %s" % (self.user, self.type, self.comment) 
