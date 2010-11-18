084badb9613758a0976cbafbcccd1d85fb9dc7a6
Geoffrey Hing <ghing@geoffsnotebook.(none)>
2010-11-18 13:30:19 -0600

    Added test to see if a single question with one answer with one upvote returns that one top answer.

--- core/tests.py
+++ core/tests.py
@@ -63,6 +63,8 @@ class QuestionTestCase(TestCase):
             relation_type = 'reply')
         reply_relation.save()
 
+        return answer
+
     def _respond_positively(self, user_profile, question):
         """Utility method to respond positively to a question."""
         comment_response = CommentResponse(user=user_profile, \
@@ -250,25 +252,50 @@ class TopAnswersTestCase(QuestionTestCase):
         
         self.assertEqual(len(top_answers), 0);
 
-    #def test_top_question_one_question_two_answers(self):
-        #topic = self._topic
-#
-        #user1_profile = UserProfile.objects.get(user__username="user1")
-        #user2_profile = UserProfile.objects.get(user__username="user2")
-        #user3_profile = UserProfile.objects.get(user__username="user3")
-        #user4_profile = UserProfile.objects.get(user__username="user4")
-        #user5_profile = UserProfile.objects.get(user__username="user5")
-#
-        #question = self._ask_question(topic=topic, text="How many wards is Chinatown in?", \
-            #user_profile=user1_profile)
-        #self._answer_question(user_profile=user2_profile, question=question, \
-            #clip_url="http://chicagojournal.com/news/04-28-2010/Boundary_lines", \
-            #clip_text="Chinatown, Bridgeport and McKinley Park - home to much of the city's Chinese-descended and immigrant populations - are politically fragmented, split between four city wards, four state representative districts, three state senate districts and three U.S. congressional districts.", \
-            #comment_text="Chinatown is split between four wards.")
-        #self._answer_question(user_profile=user3_profile, question=question, \
-            #clip_url="http://www.chicagojournal.com/News/10-20-2010/Daley%27s_departure_opens_door_for_competitive_25th_Ward_race", \
-            #clip_text="Candidates have until the third week of November to file papers with City Hall - in 2007, six candidates ran for 25th Ward Alderman with Morfin finishing second. The 25th Ward is made up of the predominantly Latino Pilsen neighborhood where Solis and his two initial challengers all reside. But it also includes parts of Chinatown, Little Italy and the Tri-Taylor area. ", \
-#
-            #comment_text="I don't know, but this article says the 25th ward contains parts of Chinatown.")
-#
-        #self.assertEqual(question.num_answers(), 2);
+    def test_one_question_two_answers_no_upvotes(self):
+        topic = self._topic
+
+        user1_profile = UserProfile.objects.get(user__username="user1")
+        user2_profile = UserProfile.objects.get(user__username="user2")
+        user3_profile = UserProfile.objects.get(user__username="user3")
+        user4_profile = UserProfile.objects.get(user__username="user4")
+        user5_profile = UserProfile.objects.get(user__username="user5")
+
+        question = self._ask_question(topic=topic, text="How many wards is Chinatown in?", \
+            user_profile=user1_profile)
+        self._answer_question(user_profile=user2_profile, question=question, \
+            clip_url="http://chicagojournal.com/news/04-28-2010/Boundary_lines", \
+            clip_text="Chinatown, Bridgeport and McKinley Park - home to much of the city's Chinese-descended and immigrant populations - are politically fragmented, split between four city wards, four state representative districts, three state senate districts and three U.S. congressional districts.", \
+            comment_text="Chinatown is split between four wards.")
+        self._answer_question(user_profile=user3_profile, question=question, \
+            clip_url="http://www.chicagojournal.com/News/10-20-2010/Daley%27s_departure_opens_door_for_competitive_25th_Ward_race", \
+            clip_text="Candidates have until the third week of November to file papers with City Hall - in 2007, six candidates ran for 25th Ward Alderman with Morfin finishing second. The 25th Ward is made up of the predominantly Latino Pilsen neighborhood where Solis and his two initial challengers all reside. But it also includes parts of Chinatown, Little Italy and the Tri-Taylor area. ", \
+
+            comment_text="I don't know, but this article says the 25th ward contains parts of Chinatown.")
+
+        top_answers = topic.top_answers()
+        
+        self.assertEqual(len(top_answers), 0);
+
+    def test_one_question_one_answer_one_upvote(self):
+        topic = self._topic
+
+        user1_profile = UserProfile.objects.get(user__username="user1")
+        user2_profile = UserProfile.objects.get(user__username="user2")
+        user3_profile = UserProfile.objects.get(user__username="user3")
+        user4_profile = UserProfile.objects.get(user__username="user4")
+        user5_profile = UserProfile.objects.get(user__username="user5")
+
+        question = self._ask_question(topic=topic, text="How many wards is Chinatown in?", \
+            user_profile=user1_profile)
+        answer = self._answer_question(user_profile=user2_profile, question=question, \
+            clip_url="http://chicagojournal.com/news/04-28-2010/Boundary_lines", \
+            clip_text="Chinatown, Bridgeport and McKinley Park - home to much of the city's Chinese-descended and immigrant populations - are politically fragmented, split between four city wards, four state representative districts, three state senate districts and three U.S. congressional districts.", \
+            comment_text="Chinatown is split between four wards.")
+
+        self._respond_positively(user3_profile, answer)
+
+        top_answers = topic.top_answers()
+        
+        self.assertEqual(len(top_answers), 1);
+
