731274a76860d54caa3fa78153c388b46808f16e
Geoffrey Hing <geoff@terrorware.com>
2010-12-03 14:14:32 -0600

    Enabled revealing a list of ids of comments that a user has commented on to the topic view template.

--- core/views.py
+++ core/views.py
@@ -396,15 +396,13 @@ def topic(request, whichtopic=1):
         template_dict['topic'] = topic 
         template_dict['comments_to_show'] = topic.comments_to_show()
        
-        # TODO: Uncomment these lines when I've implemented 
-        # Topic.user_voted_comments
         # - Geoff Hing <geoffhing@gmail.com> 2010-12-02
-        # Get a list of comments that a user has voted on. 
-#        if request.user.is_anonymous():
-#            template_dict['user_voted_comments'] = None
-#        else:
-#            template_dict['user_voted_comments'] = \
-#                topic.user_voted_comments(user_profile)
+        # Get a list of comment ids of comments that a user has voted on. 
+        if request.user.is_anonymous():
+            template_dict['user_voted_comment_ids'] = None
+        else:
+            template_dict['user_voted_comment_ids'] = \
+                topic.user_voted_comment_ids(user_profile)
 
     except: 
         # No topic loaded
