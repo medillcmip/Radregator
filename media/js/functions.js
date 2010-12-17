e3a4d8b590cde4b72452e44108c8fa4b65f5c498
shaneshifflett <shifflett.shane@gmail.com>
2010-12-17 14:18:29 -0600

    realized the iterator needed to be outside the try catch, also, it appears that by escaping even a single element the timeline is no longer drawing... will defer to andrew

--- media/js/functions.js
+++ media/js/functions.js
@@ -973,7 +973,6 @@ function initiateTopicTimeline() {
 
 
 //answerdata["20101217-1"] = {author: null,bgcolor: "rgb(31, 153, 31)",clip: null,date: "Dec. 17",link: undefined,month: "Dec.",pageanchor: undefined,popularity: "0",source: null,title: null,year: "2010"};
-//doop, what? cmon
 //END TESTING ISSUE 126
 
 	answerdata = sortThisArray(answerdata);
@@ -1089,9 +1088,10 @@ function initiateTopicTimeline() {
                 window.open(answerdata[slicestamp]["link"]);
             });
                 
-            iterator++;	
 
         }catch(e){}//not all articles have data populated 
+
+        iterator++;	
 	});
 }
 
