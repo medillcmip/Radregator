93dcbc5831a883185239fa0f28fa9e88e1442de4
shaneshifflett <shifflett.shane@gmail.com>
2011-02-07 19:26:49 -0600

    updated to accept input so we don't clobber db backups between the different servers

--- dbbackup.sh
+++ dbbackup.sh
@@ -1,10 +1,14 @@
-
 #!/bin/bash
+if [ -z "$1" ]; then
+    echo "USAGE: $0 <name-of-database-key>"
+    echo "(specify a db key to preced the file name so we don't overwrite other backups)"
+    exit
+fi
 
-outfile=radbackups/`date "+%Y%m%d".json`
+outfile=radbackups/`date "+%Y%m%d"$1.json`
 
 # Dump the data
-./manage.py dumpdata auth clipper core tagger users  > $outfile
+./manage.py dumpdata > $outfile
 
 # And put it somewhere
 # This requires ~/.awssecret have "access key \n secret key"
