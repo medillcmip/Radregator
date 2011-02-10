99fa19b4d8f4b5948a73c74f30af231ec981d67b
shaneshifflett <shifflett.shane@gmail.com>
2011-02-10 15:11:58 -0600

    fab files, updated args

--- fabfile.py
+++ fabfile.py
@@ -127,7 +127,8 @@ def git_pull(git_remote=None,git_branch=None):
         env.git_branch = git_branch
     run("cd %s/radregator; git pull %s %s" % \
         (env.base_dir, env.git_remote, env.git_branch))
-    run("cd %s/radregator; chmod 775 dbbackup.sh")
+    run("chmod 775 $s/radregator/dbbackup.sh" % \
+        (env.base_dir))
 
 def install_local_settings(db_password, fb_api_id, fb_api_key, fb_secret_key):
     require("hosts", provided_by=[staging, production, testing])
