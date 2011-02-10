545f4fa4477840f77e937bfb9b7ebc94dec6e2c5
shaneshifflett <shifflett.shane@gmail.com>
2011-02-10 14:39:35 -0600

    updated to clear tables without needed to own a schema

--- clearalltables.sh
+++ clearalltables.sh
@@ -18,4 +18,10 @@ fi
 DBNAME=$1
 USERNAME=$2
 
-psql $DBNAME $USERNAME < dropalltables.sql
+psql -t $DBNAME $USERNAME -c "SELECT 'DROP TABLE ' || n.nspname || '.' ||
+c.relname || ' CASCADE;' FROM pg_catalog.pg_class AS c LEFT JOIN
+pg_catalog.pg_namespace AS n ON n.oid = c.relnamespace WHERE relkind =
+'r' AND n.nspname NOT IN ('pg_catalog', 'pg_toast') AND
+pg_catalog.pg_table_is_visible(c.oid)" > radbackups/droptables
+
+psql $DBNAME $USERNAME -f radbackups/droptables
